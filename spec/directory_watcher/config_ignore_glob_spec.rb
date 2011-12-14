06d81fec20d4661ae62f2de96fc126775b76f278
kbl <m.pietraszek@gmail.com>
2011-12-14 14:25:18 +0100

    additional specs for ignore globs

--- spec/directory_watcher/config_ignore_glob_spec.rb
+++ spec/directory_watcher/config_ignore_glob_spec.rb
@@ -5,27 +5,42 @@ describe DirectoryWatcher do
 
     context "[#{scanner}]" do
 
-      subject {
-        options = default_options.merge(scanner: scanner, glob: '**/*.rb', ignore_glob: '**/ignored.rb')
+      let(:scenario_single) {
+        options = default_options.merge(scanner: scanner, ignore_glob: 'ignored.rb')
+        watcher = DirectoryWatcher.new(@scratch_dir, options)
+
+        DirectoryWatcherSpecs::Scenario.new(watcher)
+      }
+
+      let(:scenario_multiple) {
+        options = default_options.merge(scanner: scanner, ignore_glob: %w(*.rb *.py))
         watcher = DirectoryWatcher.new(@scratch_dir, options)
 
         DirectoryWatcherSpecs::Scenario.new(watcher)
       }
 
       it 'should ignore files which are specified in ignore glob' do
-        pending
-
         matching = scratch_path( 'matched.rb' )
         ignored = scratch_path( 'ignored.rb' )
 
-        subject.run_and_wait_for_event_count(1) do
+        scenario_single.run_and_wait_for_event_count(1) do
           touch( ignored )
           touch( matching, Time.now - 5 )
         end.run_and_wait_for_event_count(1) do
           touch( matching )
         end.stop
 
-        subject.events.should be_events_like( [[ :added, 'matched.rb' ], [ :modified, 'matched.rb' ]] )
+        scenario_single.events.should be_events_like( [[ :added, 'matched.rb' ], [ :modified, 'matched.rb' ]] )
+      end
+
+      it 'should ignore files which are specified in ignore glob' do
+        files = %w(matched1.txt matched2.csv ignored.py ignored.rb)
+
+        scenario_multiple.run_and_wait_for_event_count(2) do
+          files.each { |f| touch( scratch_path( f ) ) }
+        end.stop
+
+        scenario_multiple.events.should be_events_like( [[ :added, 'matched1.txt' ], [ :added, 'matched2.csv' ]] )
       end
     end
 
